1e056c386f09209c7c95a5a8241a7d2cf825a94e
Hotrung2311 <trungho2311@gmail.com>
2020-05-06 08:12:21 +0700

    Validate email

---
+++ Test/EmailExampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailExampleTest {
+
+    @Test
+    @DisplayName("Test case 1")
+    void validate1() {
+        
+    }
+}
